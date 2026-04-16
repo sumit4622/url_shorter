2e24ea85cdfa7e05068c6a823a5fd25087d7b313
sumit4622 <sumit.ray9299@gmail.com>
2026-04-16 16:31:53 +0545

    setup done

--- url_shorter/account/forms.py
+++ url_shorter/account/forms.py
@@ -3,7 +3,7 @@ from .models import User
 from django.core.exceptions import ValidationError
 
 class registration_form(forms.ModelForm):
-    password = forms.CharField(widget=forms.PasswordInput())
+    confirm_password = forms.CharField(label='confirm_password', widget=forms.PasswordInput)
     
     class Meta:
         model = User
@@ -19,6 +19,23 @@ class registration_form(forms.ModelForm):
         phonenumber = self.cleaned_data.get('phonenumber')
         if User.objects.filter(phonenumber=phonenumber).exists():
             raise ValidationError("A phone number is already taken.")
-        
         return phonenumber
-      
+        
+    def check_password(self):
+        password = self.cleaned_data.get('password')
+        confirm_password = self.cleaned_data.get('confirm_password')
+        
+        if password != confirm_password:
+            raise ValidationError("password must be same.")
+        
+        return password 
+    
+class login_form(forms.ModelForm):
+    
+    class Meta:
+        model = User
+        fields = ['email' ,'password']
+        
+    def check_email(self):
+        if self.email != User.email:
+            raise ValidationError("Account not created.")      
--- url_shorter/account/urls.py
+++ url_shorter/account/urls.py
@@ -4,6 +4,8 @@ from account import views
 
 urlpatterns = [
     path('sign-in/', views.login, name="login_user"),
-    path('sign-up/', views.register, name='register_user'),
+    path('sign-up/', views.register, name="register_user"),
+    path('forget-password/', views.forgetpassword, name="forget_password"),
+    path('verify-otp/', views.verify_otp, name="verify_otp"),
     
 ]
